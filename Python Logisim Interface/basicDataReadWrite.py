605ad0e8e04ae39b35c36c6807ce64b52473aed3
SMDHuman <48795375+SMDHuman@users.noreply.github.com>
2024-01-10 02:48:13 +0300

    ALU is Done

--- Python Logisim Interface/basicDataReadWrite.py
+++ Python Logisim Interface/basicDataReadWrite.py
@@ -10,21 +10,14 @@ def binary_to_gray_op(n):
 
 logi = pli.LogisimInterface()
 
-while(not keyboard.is_pressed("enter")):pass
-while(keyboard.is_pressed("enter")):pass
-
 logi.scanCircuit()
-print(logi.inputValues)
 
-while(not keyboard.is_pressed("enter")):pass
-while(keyboard.is_pressed("enter")):pass
-
-for i in range(256):
+bits = 8
+for i in range(2**bits):
 	x = bin(i)[2:]
 	x = binary_to_gray_op(x)
 	x = [int(n) for n in x]
-	x = [0]*(8-len(x)) + x
+	x = [0]*(bits-len(x)) + x
 
-	logi.setInput(8, 0)
 	logi.setAllInputs(x[::-1])
-	logi.setInput(8, 1)
+	print(x[::-1], logi.getAllOutputs())
--- Python Logisim Interface/pyLogisimInterface.py
+++ Python Logisim Interface/pyLogisimInterface.py
@@ -1,5 +1,7 @@
 import pyautogui
+import mouse
 import os
+import time
 
 class LogisimInterface:
 	inputLocs = []
@@ -9,6 +11,7 @@ class LogisimInterface:
 	outputValues = []
 
 	inputImgs = ["images/button0.png", "images/button1.png", "images/buttonX.png"]
+	outputImgs = ["images/led0.png", "images/led1.png", "images/ledX.png"]
 	handToolImg = "hand.png"
 
 	inputClickCount = [[0, 1, 2], [2, 0, 1], [1, 2, 0]]
@@ -25,27 +28,48 @@ class LogisimInterface:
 	def scanCircuit(self):
 		input1Locs = self._locsToList(pyautogui.locateAllOnScreen(self.inputImgs[1], confidence = 0.98))
 		for pos in input1Locs:
-			pyautogui.moveTo(pos.left + pos.width/2, pos.top + pos.height/2)
-			pyautogui.click()
+			mouse.move(pos.left + pos.width/2, pos.top + pos.height/2)
+			mouse.click()
 
 		inputXLocs = self._locsToList(pyautogui.locateAllOnScreen(self.inputImgs[-1], confidence = 0.98))
 		for pos in inputXLocs:
-			pyautogui.moveTo(pos.left + pos.width/2, pos.top + pos.height/2)
-			pyautogui.click()
+			mouse.move(pos.left + pos.width/2, pos.top + pos.height/2)
+			mouse.click()
 
 
 		self.inputLocs = self._locsToList(pyautogui.locateAllOnScreen(self.inputImgs[0], confidence = 0.98))
 		self.inputValues = [0] * len(self.inputLocs)
 
+
+		self.outputLocs += self._locsToList(pyautogui.locateAllOnScreen(self.outputImgs[0], confidence = 0.98))
+		self.outputLocs += self._locsToList(pyautogui.locateAllOnScreen(self.outputImgs[1], confidence = 0.98))
+		self.outputLocs += self._locsToList(pyautogui.locateAllOnScreen(self.outputImgs[-1], confidence = 0.98))
+		
+		self.outputLocs = sorted(self.outputLocs , key=lambda k: [k[1], k[0]])
+		
+		for i, pos in enumerate(self.outputLocs):
+			x = int(pos.left + pos.width//2)+3
+			y = int(pos.top + pos.height//2)
+
+			self.outputLocs[i] = (x, y)
+
+			self.outputValues.append(0)
+			self.getValue(i)
+
 	def setInput(self, index, value):
 		if(self.inputValues[index] != value):
 			pos = self.inputLocs[index]
-			pyautogui.moveTo(pos.left + pos.width/2, pos.top + pos.height/2)
-			pyautogui.click()
+			mouse.move(pos.left + pos.width/2, pos.top + pos.height/2)
+			mouse.click()
 			self.inputValues[index] = value
 
 	def getValue(self, index):
-		...  
+		im = pyautogui.screenshot()
+		green = im.getpixel(self.outputLocs[index])[1]
+
+		self.outputValues[index] = {100:0, 210:1, 40:-1}[green]
+
+		return(self.outputValues[index])
 
 	def setAllInputs(self, values):
 		if(len(self.inputValues) < len(values)):
@@ -57,13 +81,16 @@ class LogisimInterface:
 			self.setInput(i, v)
 
 	def getAllOutputs(self):
-		...
+		values = [0]*len(self.outputValues)
+		for i in range(len(self.outputValues)):
+			values[i] = self.getValue(i)
+		return(values)
 
 if(__name__ == "__main__"):
 	import keyboard
 	logi = LogisimInterface()
 
-	while(not keyboard.is_pressed("enter")):pass
-	while(keyboard.is_pressed("enter")):pass
-
 	logi.scanCircuit()
+
+	print(logi.outputValues)
+	
